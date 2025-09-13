ebb5e4c95a210f0e0e18d820645acddb840f3bd7
Patrick <patrick@failmail.dev>
2025-09-13 11:15:21 +0200

    WIP: now at least works until it calls rage

--- tests/default.nix
+++ tests/default.nix
@@ -9,8 +9,22 @@
     { pkgs, system, ... }:
     let
       nixos-lib = import (pkgs.path + "/nixos/lib") { };
-      flake = ../examples/flake-parts;
-      agenix = (import "${flake}/flake.nix").outputs (inputs);
+      testFlake = ./cases/flake-parts;
+      agenix = callFlake "${testFlake}/flake.nix";
+      callFlake =
+        src:
+        lib.fix (
+          flake:
+          (import src).outputs (
+            inputs
+            // {
+              self = flake // {
+                inherit inputs;
+                outPath = "${testFlake}";
+              };
+            }
+          )
+        );
     in
     {
       checks = {
@@ -31,24 +45,46 @@
                 environment.etc.setupScript = {
                   mode = "777";
                   source = pkgs.writeText "test" ''
-                    cp -r  ${flake} /tmp/test
+                    cp -r  ${testFlake} /tmp/test
                     chmod -R 777 /tmp/test
                     cd /tmp/test
                     export NIX_CONFIG="extra-experimental-features = flakes nix-command"
                     nix flake lock --offline --override-input agenix-rekey ${../.} ${
-                      lib.concatMapAttrsStringSep " " (name: x: "--override-input ${name} ${x.outPath}") inputs
+                      lib.concatMapAttrsStringSep " " (name: x: "--override-input ${name} ${x.outPath}") (
+                        lib.filterAttrs (name: _: name != "self" && name !="agenix-rekey") inputs
+                      )
                     }
 
                   '';
                 };
                 environment.etc.testScript = {
                   mode = "777";
-                  source = pkgs.writeText "test" ''
-                    cd /tmp/test
-                    export NIX_CONFIG="extra-experimental-features = flakes nix-command"
-                    ${lib.getExe agenix.packages.${system}.agenix} rekey
-                    #nix run --offline .#packages.${system}.agenix
-                  '';
+                  source = lib.getExe (
+                    pkgs.writeShellApplication {
+                      name = "test";
+                      runtimeInputs = [
+                        # # Don't actually use this. It's just to make sure the dependencies
+                        # # are in the nix store of the vm
+                        agenix.packages.${system}.agenix
+                        agenix.agenix-rekey.${system}.rekey
+                        # For writeshellapplication
+                        pkgs.shellcheck-minimal
+                        pkgs.shellcheck-minimal
+                        # # Force the dependencies to also be part of the store
+                        agenix.agenix-rekey.${system}.rekey.drvPath
+                      ];
+                      text = ''
+                        cd /tmp/test
+                        export NIX_CONFIG="extra-experimental-features = flakes nix-command"
+                        ls -lah "$(which shellcheck)"
+                        alias nix="nix --offline"
+                        nix run --offline .#packages.${system}.agenix -- rekey
+                        nix eval --offline .#nixosConfigurations.node.${system}.config.system.activationScripts.agenixNewGeneration.text | bash
+                        nix eval --offline .#nixosConfigurations.node.${system}.config.system.activationScripts.agenixInstall.text | bash
+                        cat /run/agenix/root-pw-hash
+                      '';
+                    }
+                  );
                 };
               };
             testScript = ''
